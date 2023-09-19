4d4b2939e33cc9addd044411c571281324e61715
voh.vinh803 <voh.vinh803@gmail.com>
2023-09-19 23:25:19 +0700

    create navbar

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import React from "react";
+import { Browser as Router, Routes, Route } from "react-router-dom"
+import { Navbar } from "./components/navbar";
+
+function App() {
+    return (
+        <div className="App">
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" />
+                    <Route path="/cart" />
+                </Routes>
+            </Router>
+        </div>
+    )
+}
+
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import reactDom from "react-dom";
-import App from "./components/app"
+import App from "./App"
 
 reactDom.render(<App />, document.getElementById("root"));
