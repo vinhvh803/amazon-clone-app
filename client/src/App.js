d4b8534cb10e8abdd07438246f702852c322089d
voh.vinh803 <voh.vinh803@gmail.com>
2023-09-28 11:15:00 +0700

    add shop page and cart page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Navbar } from "./components/navbar.jsx";
+import { Shop } from "./pages/shop/shop.jsx";
+import { Cart } from "./pages/cart/cart.jsx";
 
 function App() {
     return (
@@ -9,8 +11,8 @@ function App() {
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path="/" />
-                    <Route path="/cart" />
+                    <Route path="/" element={<Shop />} />
+                    <Route path="/cart" element={<Cart />} />
                 </Routes>
             </Router>
         </div>
