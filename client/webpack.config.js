fa53696cc96cae2f01369e9ab38a71081952fada
voh.vinh803 <voh.vinh803@gmail.com>
2023-09-28 11:03:36 +0700

    add navbar.css || edit webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -46,6 +46,14 @@ module.exports = {
                 test: /\.js|\.jsx$/,   //kind of file extension this rule should look for and apply in test
                 exclude: /node_modules/, //folder to be excluded
                 use: 'babel-loader' //loader which we are going to use
+            },
+            {
+                test: /\.css$/,   //kind of file extension this rule should look for and apply in test
+                //exclude: /node_modules/, //folder to be excluded
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ] //loader which we are going to use
             }
         ]
     }
